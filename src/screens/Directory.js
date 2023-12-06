f5a5cb9d0cb7debb459428d818f334935a62e5ae
davidmarint <maicolmarin0126@gmail.com>
2023-12-05 23:07:11 -0500

    segundo commit

---
+++ src/screens/Directory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Directory = () => {
+    return (
+    <View>
+        <Text>Emergency directory</Text>
+    </View>
+    );
+}
+
+export default Directory;
---
+++ src/screens/Home.js
@@ -0,0 +1,15 @@
+import tw from 'twrnc'
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+const Home = () => {
+    return (
+    <SafeAreaView>
+        <View>
+        <Text>Home Emergency directory </Text>
+        </View>
+    </SafeAreaView>
+    );
+}
+
+export default Home;
